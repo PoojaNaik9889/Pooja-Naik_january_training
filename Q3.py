8eacf3cc2ae272acd79c8f0dbddcab3d37d9ba8d
PoojaNaik9889 <161812268+PoojaNaik9889@users.noreply.github.com>
2026-01-17 19:32:24 +0530

    chore: removed Q3.py from root

--- Q3.py
+++ Q3.py
@@ -1,10 +1,1 @@
-#Fix the Default Parameter Bug
 
-def add_item(item, items=None):
-    if items is None:
-        items = []
-    items.append(item)
-    return items
-
-print(add_item(1))
-print(add_item(2))
